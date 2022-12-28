1dbff86e7148b0d8cf5e0a4bf2e94361dd72c77f
Adil <adilkhanbala04@gmail.com>
2022-12-29 00:54:01 +0600

    Homework JS

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "using strict"
+
+const NumberOfFilms = +prompt("Сколько фильмов вы смотрели?", "" )
+
+
+const personalMovieDB = {
+    connt: NumberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt("Насколько оцените его?", ""),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d  = prompt("Насколько оцените его?", "");
+
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
